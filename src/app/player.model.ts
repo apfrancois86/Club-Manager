a74a79962441543514d32830c594ee86fc550a53
Alex Francois <apfrancois86@gmail.com>
2017-01-27 09:50:18 -0800

    add sample json file for firebase database

--- src/app/player.model.ts
+++ src/app/player.model.ts
@@ -1,3 +1,3 @@
 export class Player {
-    constructor (public name: string, public image: string, public position: string) { }
+    constructor (public name: string, public image: string, public position: string, public college: string) { }
 }
