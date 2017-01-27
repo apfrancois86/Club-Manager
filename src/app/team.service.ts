a521f0f5397ab1cc943a25bc5b2cbca77ae1add4
Alex Francois <apfrancois86@gmail.com>
2017-01-27 10:15:26 -0800

    add team component to display player names

--- src/app/team.service.ts
+++ src/app/team.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Player } from './player.model';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Injectable()
 export class TeamService {
+  player: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private angularFire: AngularFire) {
+    this.player = angularFire.database.list('players');
+  }
 
+  getPlayers(){
+    return this.player;
+  }
+
+  addPlayer(newPlayer: Player) {
+    this.player.push(newPlayer);
+  }
+
+  getPlayerById(playerId: string){
+    return this.angularFire.database.object('players/' + playerId);
+  }
 }
--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Player } from '../player.model';
+import { Router } from '@angular/router';
+import { TeamService } from '../team.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
-  styleUrls: ['./team.component.css']
+  styleUrls: ['./team.component.css'],
+  providers: [TeamService]
 })
+
 export class TeamComponent implements OnInit {
+  player: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
-  constructor() { }
+  constructor(private router: Router, private teamService: TeamService){}
 
-  ngOnInit() {
+  ngOnInit(){
+    this.player = this.teamService.getPlayers();
   }
+
+  goToDetailPage(clickedPlayer) {
+     this.router.navigate(['players', clickedPlayer.$key]);
+   };
 
 }
